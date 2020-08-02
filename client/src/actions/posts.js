bd3d6bf830627be282dd35234263e069e0799319
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2020-08-02 11:22:21 +0530

    created actions components helpers reducers store css

---
+++ client/src/actions/posts.js
@@ -0,0 +1,23 @@
+import { UPDATE_POSTS } from "./actionTypes";
+import { APIUrls } from "../helpers/urls";
+
+export function fetchPosts() {
+  return (dispatch) => {
+    const url = APIUrls.fetchPosts();
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch(updatePosts(data.data.posts));
+      });
+  };
+}
+
+export function updatePosts(posts) {
+  return {
+    type: UPDATE_POSTS,
+    posts,
+  };
+}
---
+++ client/src/components/App.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { connect } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import PropTypes from "prop-types";
+
+import { fetchPosts } from "../actions/posts";
+import { Home, Navbar, Page404, Login, Signup } from "./";
+import * as jwtDecode from "jwt-decode";
+import { authenticateUser } from "../actions/auth";
+
+const Settings = () => <div>Setting</div>;
+
+const PrivateRoute = (privateRouteProps) => {
+  const { isLoggedin, path, component: Component } = privateRouteProps;
+
+  return (
+    <Route
+      path={path}
+      render={(props) => {
+        return isLoggedin ? <Component {...props} /> : <Redirect to="/login" />;
+      }}
+    />
+  );
+};
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchPosts());
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const user = jwtDecode(token);
+
+      console.log("user", user);
+      this.props.dispatch(
+        authenticateUser({
+          email: user.email,
+          _id: user._id,
+          name: user.name,
+        })
+      );
+    }
+  }
+
+  render() {
+    const { posts, auth } = this.props;
+    return (
+      <Router>
+        <div>
+          <Navbar />
+
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={(props) => {
+                return <Home {...props} posts={posts} />;
+              }}
+            />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <PrivateRoute
+              path="/settings"
+              component={Settings}
+              isLoggedin={auth.isLoggedin}
+            />
+            <Route component={Page404} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    posts: state.posts,
+    auth: state.auth,
+  };
+}
+
+App.propTypes = {
+  posts: PropTypes.array.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
