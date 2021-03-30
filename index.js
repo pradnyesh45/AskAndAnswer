a6c20fef654a502eb96fce8331bae3115478d497
pradnyesh45 <pradnyeshaglawe12@gmail.com>
2021-03-30 08:08:44 +0530

    configured again for hosting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 const passport = require("passport");
 // const passportJWT = require("./config/passport-jwt-strategy");
 const bodyParser = require("body-parser");
